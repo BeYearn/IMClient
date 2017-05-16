d87f90d4b043afe10269bee6428f9f3d76a92724
BY <beyearn@gmail.com>
2017-05-16 11:33:10 +0800

    一些注释

--- imsdk/src/main/java/com/emagroup/imsdk/MsgBean.java
+++ imsdk/src/main/java/com/emagroup/imsdk/MsgBean.java
@@ -6,14 +6,14 @@ package com.emagroup.imsdk;
 
 public class MsgBean {
     private String appId;
-    private String fName;
-    private String fuid;
-    private String handler;
-    private String msg;
-    private String msgId;
-    private String tID;
+    private String fName;   //发送这个消息的人的名字
+    private String fuid;     //发送这个消息的人的id
+    private String handler;  //2私聊 3长频道 4短频道
+    private String msg;      // 消息内容
+    private String msgId;    // 消息时间
+    private String tID;      // 接受者/频道 的id
     private String ext; //透传字段
-    private String mark; //用来在长连接传输时对应每条信息的
+    private String mark; //使用者不用管，用来在长连接传输时对应每条信息的
 
     public String getAppId() {
         return appId;
